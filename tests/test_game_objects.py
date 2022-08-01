8c4d7a7b9baafd958ea5a3e4e051d8638b82e7ce
Bijal Thanawala <bijalthanawala@users.noreply.github.com>
2022-08-01 18:12:59 +1000

    test: Moved all GameObject tests into a single file

--- tests/test_game_objects.py
+++ tests/test_game_objects.py
@@ -3,6 +3,26 @@ from snake_ladder_simulation import GameObject, Snake, Ladder, Game
 
 
 class Test_add_game_objects:
+    def test_GameObject_short(self):
+        with pytest.raises(Exception) as excinfo:
+            gameobject = GameObject(high=50, low=45)
+        assert GameObject.EXCEPTION_MIN_LENGTH in str(excinfo.value)
+
+    def test_GameObject_high_point_invalid(self):
+        with pytest.raises(Exception) as excinfo:
+            gameobject = GameObject(high=30, low=50)
+        assert GameObject.EXCEPTION_HIGH_POINT_INVALID in str(excinfo.value)
+
+    def test_Snake_good(self):
+        snake = Snake(high=50, low=30)
+        assert snake.activation_point == 50
+        assert snake.termination_point == 30
+
+    def test_Ladder_good(self):
+        ladder = Ladder(high=50, low=30)
+        assert ladder.activation_point == 30
+        assert ladder.termination_point == 50
+
     @pytest.fixture
     def game(self):
         return Game()
